08c96af9bc57a07a126f789e9fd44b88fe864425
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-01-30 21:33:55 +0530

    Code

--- January-2024/30-01-24_nextjs_page_router/page_router/src/pages/about.js
+++ January-2024/30-01-24_nextjs_page_router/page_router/src/pages/about.js
@@ -1,7 +1,8 @@
-
+import Link from 'next/link';
 
 export default function About(){
     return (<>
        <h1>This is About Page</h1>
+       <h5><Link href="/">Go Back to Home Page</Link></h5>
     </>)
 }
--- January-2024/30-01-24_nextjs_page_router/page_router/src/pages/index.js
+++ January-2024/30-01-24_nextjs_page_router/page_router/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-            <h1>Hello World</h1>
+           <nav>
+               <ul>
+                <li>
+                    <Link href="/post">Post</Link>
+                </li>
+                <li>
+                    <Link href="/about">About</Link>
+                </li>
+               </ul>
+           </nav>
       </main>
     </>
   );
