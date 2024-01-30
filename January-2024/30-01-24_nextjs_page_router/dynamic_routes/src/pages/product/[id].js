33425966b2de3c281db550cf732142b6d791d43e
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-01-30 22:51:49 +0530

    Code

--- January-2024/30-01-24_nextjs_page_router/dynamic_routes/src/pages/product/[id].js
+++ January-2024/30-01-24_nextjs_page_router/dynamic_routes/src/pages/product/[id].js
@@ -1,5 +1,11 @@
+import { useRouter } from "next/router";
+
 export default function ProductDetails(){
+    
+       const router = useRouter();
+       const {id} = router.query;
+
         return(<>
-          <h1>Product Details</h1>
+          <h1>Product Details - {id}</h1>
         </>)
 }
