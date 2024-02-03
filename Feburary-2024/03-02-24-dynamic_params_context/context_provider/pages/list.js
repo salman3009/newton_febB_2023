d3c08a58bc833519ef4a4e9d67de297cb8bfaa4b
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-02-03 22:57:48 +0530

    Code

--- Feburary-2024/03-02-24-dynamic_params_context/context_provider/pages/list.js
+++ Feburary-2024/03-02-24-dynamic_params_context/context_provider/pages/list.js
@@ -1,11 +1,13 @@
 
-
+import { useConsumer } from "../AuthContext";
 
 const list =()=>{
-    
+     
+    const name = useConsumer();
+
      return (<>
         <h1>List Component</h1>
-        <h1>The given name is:</h1>
+        <h1>The given name is: {name}</h1>
        </>)
 }
 
--- Feburary-2024/03-02-24-dynamic_params_context/context_provider/pages/login.js
+++ Feburary-2024/03-02-24-dynamic_params_context/context_provider/pages/login.js
@@ -1,8 +1,10 @@
 
 import {useState} from 'react';
-
+import { useConsumer } from '../AuthContext';
 const login =()=>{
      const [getName,setName] = useState(null);
+     const name = useConsumer();
+
 
      const onChangeHandler=(event)=>{
         setName(event.target.value);
@@ -12,6 +14,7 @@ const login =()=>{
         <h1>Login Component</h1>
         <input type="text" onChange={onChangeHandler}/>
         <h2>Name Display:{getName}</h2>
+        <h3>{name}</h3>
        </>)
 }
 
