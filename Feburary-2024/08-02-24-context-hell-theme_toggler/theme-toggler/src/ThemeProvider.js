f6a74ef33f42ed440202e4377fe7dfdbceba56a3
salman3009 <salman444g@gmail.com>
2024-02-08 22:40:32 +0530

    code

--- Feburary-2024/08-02-24-context-hell-theme_toggler/theme-toggler/src/ThemeProvider.js
+++ Feburary-2024/08-02-24-context-hell-theme_toggler/theme-toggler/src/ThemeProvider.js
@@ -1,7 +1,20 @@
 
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
 
 export const ThemeProvider=(props)=>{
+     const [theme,setTheme] = useState('light');
+
+     const setThemeToggler=()=>{
+        setTheme(theme == "light"?"dark":"light");
+     }
+
       return (<>
-          {props.children}
+      <ThemeContext.Provider value={{theme,setThemeToggler}}>
+      {props.children}
+      </ThemeContext.Provider>
+         
       </>)
 }
