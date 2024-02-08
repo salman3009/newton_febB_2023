c3164129f739c691134cd7794beb696448cebcc0
salman3009 <salman444g@gmail.com>
2024-02-08 21:48:00 +0530

    code

--- Feburary-2024/08-02-24-context-hell-class-component/context-hell-solution/pages/_app.js
+++ Feburary-2024/08-02-24-context-hell-class-component/context-hell-solution/pages/_app.js
@@ -2,10 +2,10 @@
 import React from 'react';
 
 import MyComponent from '../components/MyComponent';
-import ConfigProvider from '../contexts/ConfigContext';
-import LanguageProvider from '../contexts/LanguageContext';
-import ThemeProvider from '../contexts/ThemeContext';
-import UserProvider from '../contexts/UserContext';
+import {ConfigProvider} from '../contexts/ConfigContext';
+import {LanguageProvider} from '../contexts/LanguageContext';
+import {ThemeProvider} from '../contexts/ThemeContext';
+import {UserProvider} from '../contexts/UserContext';
 
 function App() {
   return (
