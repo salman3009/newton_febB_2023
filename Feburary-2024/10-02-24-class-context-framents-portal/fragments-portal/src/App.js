7d3e4b78ab4cb2c31b5af72deeef3ec857855a20
salman3009 <salman444g@gmail.com>
2024-02-10 22:05:46 +0530

    code

---
+++ Feburary-2024/10-02-24-class-context-framents-portal/fragments-portal/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+      //  <React.Fragment>
+      //  <h1>Hello</h1>
+      //  <h2>World</h2>
+      // </React.Fragment>
+      <>
+      <h1>Hello</h1>
+      <h2>World</h2>
+     </>
+  );
+}
+
+export default App;
