6dd9f75caf98412a557047f44f44d36b4c35c29f
salman3009 <salman444g@gmail.com>
2024-02-10 22:47:28 +0530

    code

---
+++ Feburary-2024/10-02-24-class-context-framents-portal/open-modal-with-portal-solution/components/Modal.js
@@ -0,0 +1,18 @@
+
+import ReactDOM from 'react-dom';
+
+const Modal = ({ children, isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(<div className='modal'>
+
+    <div style={{ backgroundColor: 'white', padding: '2rem', position: 'relative' }} onClick={(e) => e.stopPropagation()} >// This prevents the modal from closing when clicking inside
+      {children}
+    </div>
+  </div>,
+    document.body)
+
+
+};
+
+export default Modal;
---
+++ Feburary-2024/10-02-24-class-context-framents-portal/open-modal-with-portal-solution/pages/index.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import Modal from '../components/Modal';
+
+const HomePage = () => {
+  
+  const[isModelOpen,setModalOpen] = useState(false);
+
+
+  return (
+    <div>
+      <button onClick={()=>setModalOpen(true)}>Open Modal</button>
+
+      <Modal isOpen={isModelOpen} onClose={()=>setModalOpen(false)} >
+        <h2>This is a Modal</h2>
+        <p>Using React Portals with Next.js</p>
+        <button >Close Modal</button>
+      </Modal>
+    </div>
+  );
+};
+
+export default HomePage;
