e9b932266577db715e2cedfe36c4cb2db2858dff
salman3009 <salman444g@gmail.com>
2024-02-13 22:43:35 +0530

    code

---
+++ Feburary-2024/13-02-24-forward_ref_useReducer/reducer-practice/src/App.js
@@ -0,0 +1,35 @@
+import React, { useReducer }from 'react'
+
+const App = () => {
+
+
+  const intialTaskState = { count: 0 };
+
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'add':
+        return { count: state.count + 1 }
+      case 'delete':
+        return { count: state.count>0?state.count-1:0}
+      case 'deleteAll':
+        return { count: 0 }
+    }
+  
+  }
+
+  const [taskState, dispatch] = useReducer(reducer, intialTaskState);
+  
+  return (
+    <div id="main">
+      <h2>Task Counter</h2>
+      <button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
+      <button onClick={() => dispatch({type: 'delete'})}  id="delTaskBtn">Delete a task</button>
+      <button onClick={() => dispatch({type: 'deleteAll'})}  id="delAllTaskBtn">Delete all tasks</button>
+      <h3 className="counter">Number of Tasks : {taskState.count}</h3>
+    </div>
+  )
+}
+
+
+export default App;
