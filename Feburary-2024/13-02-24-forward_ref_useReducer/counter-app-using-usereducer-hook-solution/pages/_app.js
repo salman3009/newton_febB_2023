723d7b9bcc4c5f8ee74eff2289c226d3fc108e49
salman3009 <salman444g@gmail.com>
2024-02-13 22:47:24 +0530

    code

---
+++ Feburary-2024/13-02-24-forward_ref_useReducer/counter-app-using-usereducer-hook-solution/pages/_app.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
