44a7d86082676632182ca5e23881bf3fc67baefa
salman3009 <salman444g@gmail.com>
2024-02-20 22:27:42 +0530

    code

---
+++ Feburary-2024/20-02-24-usecallback-useMemo/usememo-optimization/src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+function isPrime(num) {
+  console.log("isPrime function");
+  if (num == 0 || num == 1) {
+    return false;
+  }
+
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function armStrong(num){
+    console.log("armstrong function");
+  let sum=0;
+  let temp = num;
+  let digits = num.toString().length;
+  while(temp>0){
+      let reminder = temp%10;
+      sum+=reminder ** digits;
+      temp = parseInt(temp/10);
+  }
+   
+  return sum == num;
+}
+
+function App() {
+
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+   
+  let prime = isPrime(num1);
+  let arm = armStrong(num2);
+
+  return (
+    <div className="App">
+      <h1>{num1 && prime? "Prime number":"no"}</h1>
+      Prime Number check:<input type="text" onChange={(e)=>setNum1(e.target.value)}/>
+      <br/>
+      <h1>{num2 && arm?"Armstrong number":"no"}</h1>
+      armstrong Number check:<input type="text" onChange={(e)=>setNum2(e.target.value)}/>
+      <br/>
+    </div> 
+  );
+}
+
+export default App;
