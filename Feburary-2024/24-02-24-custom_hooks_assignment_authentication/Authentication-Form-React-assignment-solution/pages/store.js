5c3e645d3bf171ed2a943eb655a62e25edeb9faf
salman3009 <salman444g@gmail.com>
2024-02-24 22:43:16 +0530

    code

--- Feburary-2024/24-02-24-custom_hooks_assignment_authentication/Authentication-Form-React-assignment-solution/pages/store.js
+++ Feburary-2024/24-02-24-custom_hooks_assignment_authentication/Authentication-Form-React-assignment-solution/pages/store.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function Store() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem('loginStatus');
+    if (isLoggedIn != 'true') {
+      router.push('/login');
+    }
+  }, [])
+
+  const logoutHandler = () => {
+    localStorage.setItem('loginStatus', 'false');
+    router.push('/login');
+
+  }
   return (
     <div className='store'>
       <h2>Welcome to the store!</h2>
-      <button className='logout-btn'>Logout</button>
+      <button className='logout-btn' onClick={logoutHandler}>Logout</button>
     </div>
   );
 }
