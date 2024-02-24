659c1db8a737a0248acb0bd252a8e9fad0eccdcc
salman3009 <salman444g@gmail.com>
2024-02-24 22:36:35 +0530

    code

--- Feburary-2024/24-02-24-custom_hooks_assignment_authentication/Authentication-Form-React-assignment-solution/pages/login.js
+++ Feburary-2024/24-02-24-custom_hooks_assignment_authentication/Authentication-Form-React-assignment-solution/pages/login.js
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import Link from 'next/link';
+import {useRouter} from 'next/navigation';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -8,18 +9,40 @@ const Login = () => {
   const [error, setError] = useState(false);
   const [isLogIn, setIsLogIn] = useState(false);
 
+
+  const router = useRouter();
+
+
+  const handleSubmit=(e)=>{
+     e.preventDefault();
+     if(!email || !password){
+      setError(true);
+     }
+     else{
+      const user = JSON.parse(localStorage.getItem('user'));
+      if(user && user.email == email && user.password == password){
+        localStorage.setItem('loginStatus','true');
+        router.push('/store');
+      }
+      else{
+        setError(false);
+      }
+     
+     }
+  }
+
   return (
     <div className='login'>
       <h2>Login</h2>
-      <form>
-        <p className='error-para'>"Email or password is invalid"</p>
+      <form onSubmit={handleSubmit}>
+        {error &&<p className='error-para'>"Email or password is invalid"</p>}
         <div className='email-div'>
           <label htmlFor='email'>Email: </label>
-          <input type='email' id='email' />
+          <input type='email' id='email' value={email} onChange={(e)=>setEmail(e.target.value)} />
         </div>
         <div className='password-div'>
           <label htmlFor='password'>Password: </label>
-          <input type='password' id='password' />
+          <input type='password' id='password' value={password} onChange={(e)=>setPassword(e.target.value)} />
         </div>
         <button type='submit' className='login-btn'>
           Log In
