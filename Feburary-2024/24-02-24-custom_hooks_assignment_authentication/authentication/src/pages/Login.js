89805cf14a7a4dfd09d66da7f4ca81f688a10840
salman3009 <salman444g@gmail.com>
2024-02-24 21:57:29 +0530

    code

--- Feburary-2024/24-02-24-custom_hooks_assignment_authentication/authentication/src/pages/Login.js
+++ Feburary-2024/24-02-24-custom_hooks_assignment_authentication/authentication/src/pages/Login.js
@@ -1,10 +1,48 @@
-
-
-
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function Login(){
+
+    const [getEmail,setEmail] = useState('');
+    const [getPassword,setPassword] = useState('');
+    const [getError,setError] = useState('');
+
+    const navigate = useNavigate();
+
+
+    const onSubmitHandler=(e)=>{
+        e.preventDefault();
+        setError('');
+        if(!getEmail || !getPassword){
+            setError("email and password cannot be empty");
+            return;
+        }
+        const user = JSON.parse(localStorage.getItem('user'));
+        if(user && user.getEmail == getEmail && user.getPassword == getPassword){
+            navigate('/home');
+        }
+        else{
+            setError("email and password is invalid");
+            return;  
+        }
+       
+        
+    }
+
     return (<>
-     <h1>Login</h1>
+     <h1>Login Page</h1>
+     {getError && <h1>{getError}</h1>}
+     <form>
+        <div>
+            Email:<input type="email" onChange={(e)=>setEmail(e.target.value)}/>
+        </div>
+        <div>
+            Password:<input type="password" onChange={(e)=>setPassword(e.target.value)}/>
+        </div>
+        <div>
+            <button onClick={onSubmitHandler}>Login</button>
+        </div>
+     </form>
     </>)
 }
 export default Login;
