c1c04e264ff3fbb00176e2f13c496fcafecf5487
salman3009 <salman444g@gmail.com>
2024-02-25 14:16:44 +0530

    code

--- Feburary-2024/25-02-24-project-building/music-app/src/pages/Home.js
+++ Feburary-2024/25-02-24-project-building/music-app/src/pages/Home.js
@@ -1,11 +1,35 @@
 
 import Sidebar from '../components/Sidebar';
 import MusicCard from '../components/MusicCard';
-
+import {useState,useEffect} from 'react';
+import axios from 'axios';
 
 function Home(){
     
+   const [getData,setData] = useState([]);
+
+
+   useEffect(()=>{
+      musicList();
+   },[])
+
    
+   const musicList = async ()=>{
+      try{
+         const response = await axios.get('https://academics.newtonschool.co/api/v1/music/song',{
+            headers:{
+               projectID:'f104bi07c490'
+            }
+         })
+         console.log(response.data.data);
+         setData(response.data.data);
+      }catch(err){
+           console.log(err);
+      }
+     
+   }
+
+
 
     return(<>
        <div className="global-container">
