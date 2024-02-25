80b241be4636701036f37ddf795dabe3f8d4eea5
salman3009 <salman444g@gmail.com>
2024-02-25 13:44:47 +0530

    code

--- Feburary-2024/25-02-24-project-building/music-app/src/App.js
+++ Feburary-2024/25-02-24-project-building/music-app/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Library from './pages/Library';
 
 function App() {
   return (
     <>
-     <h1>Welcome app</h1>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/library" element={<Library/>}/>
+      </Routes>
+     </BrowserRouter>
     </>
   );
 }
