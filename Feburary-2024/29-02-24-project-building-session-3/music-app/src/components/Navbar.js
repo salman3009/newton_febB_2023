65c56ec44836a69a521301a50e95371c2671dfa0
salman3009 <salman444g@gmail.com>
2024-02-29 22:11:01 +0530

    code

--- Feburary-2024/29-02-24-project-building-session-3/music-app/src/components/Navbar.js
+++ Feburary-2024/29-02-24-project-building-session-3/music-app/src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
 
   return (<>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#"><i class="fa-solid fa-music"></i><span className="icon-music">Music</span></a>
+      <Link className="navbar-brand" to="/"><i class="fa-solid fa-music"></i><span className="icon-music"></span></Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -22,7 +22,7 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
           </li>
         </ul>
         <li className="nav-item dropdown my-2 my-lg-0 left-nav">
