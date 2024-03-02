b66209105214067510c96ec92c423f5f499d5250
salman3009 <salman444g@gmail.com>
2024-03-02 22:27:16 +0530

    code

--- Feburary-2024/29-02-24-project-building-session-3/music-app/src/pages/Library.js
+++ Feburary-2024/29-02-24-project-building-session-3/music-app/src/pages/Library.js
@@ -1,37 +1,53 @@
 import Sidebar from "../components/Sidebar";
 import axios from 'axios';
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { useUser } from "../providers/UserProvider";
-function Library(){
+function Library() {
 
-   const {getToken} = useUser();
+   const { getToken } = useUser();
+   const [getList, setList] = useState([]);
 
-   const listOfLibrary=()=>{
-       axios.get('https://academics.newtonschool.co/api/v1/music/favorites/like',{
+   useEffect(() => {
+      listOfLibrary();
+   }, [])
+
+   const listOfLibrary = () => {
+      axios.get('https://academics.newtonschool.co/api/v1/music/favorites/like', {
          headers: {
             projectID: 'f104bi07c490',
-            Authorization:`Bearer ${getToken}`
-          }
-       }).then((result)=>{
-           console.log(result.data)
-       }).catch((err)=>{
-           console.log(err);
-       })
+            Authorization: `Bearer ${getToken}`
+         }
+      }).then((result) => {
+         console.log(result.data.data.songs)
+         setList(result.data.data.songs);
+      }).catch((err) => {
+         console.log(err);
+      })
    }
 
 
-    return(<>
-    <div className="global-container">
-          <div className="left-sidebar">
-               <Sidebar/>
-          </div>
-          <div className="right-sidebar">
+   return (<>
+      <div className="global-container">
+         <div className="left-sidebar">
+            <Sidebar />
+         </div>
+         <div className="right-sidebar">
             <div className="music-container">
-               
+                  {getList.map((obj,index) => {
+                     return (<div  key={index} className="musicCard">
+                        <img
+                           src={obj.thumbnail}
+                           height={"150"}
+                           width={"150"}
+                           className="bannerImg"
+                        />
+                        <div className="music-title">{obj.title}</div>
+                     </div>)
+                  })}
             </div>
-            
-          </div>
-       </div>
-    </>)
- }
- export default Library;
+
+         </div>
+      </div>
+   </>)
+}
+export default Library;
